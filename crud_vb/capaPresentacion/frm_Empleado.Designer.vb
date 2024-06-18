99c00deb12b3b653a4c384dfd246e0436128fa25
JoseTorres <joseangel.tosa@outlook.com>
2024-06-17 22:26:29 -0700

    se agrego ignore

--- crud_vb/capaPresentacion/frm_Empleado.Designer.vb
+++ crud_vb/capaPresentacion/frm_Empleado.Designer.vb
@@ -22,16 +22,246 @@ Partial Class frm_Empleado
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Me.lblID_guna2 = New Guna.UI2.WinForms.Guna2HtmlLabel()
+        Me.lblNombre_guna2 = New Guna.UI2.WinForms.Guna2HtmlLabel()
+        Me.lblApellidos_guna2 = New Guna.UI2.WinForms.Guna2HtmlLabel()
+        Me.Guna2TextBox1 = New Guna.UI2.WinForms.Guna2TextBox()
+        Me.Guna2TextBox2 = New Guna.UI2.WinForms.Guna2TextBox()
+        Me.Guna2TextBox3 = New Guna.UI2.WinForms.Guna2TextBox()
+        Me.Guna2TextBox4 = New Guna.UI2.WinForms.Guna2TextBox()
+        Me.lblDireccion_guna2 = New Guna.UI2.WinForms.Guna2HtmlLabel()
+        Me.Guna2PictureBox1 = New Guna.UI2.WinForms.Guna2PictureBox()
+        Me.pboxReferencia_guna2 = New Guna.UI2.WinForms.Guna2HtmlLabel()
+        Me.btngradNuevoReg_guna2 = New Guna.UI2.WinForms.Guna2GradientButton()
+        Me.Guna2Button1 = New Guna.UI2.WinForms.Guna2Button()
+        Me.btnVerReg_guna2 = New Guna.UI2.WinForms.Guna2Button()
+        CType(Me.Guna2PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
+        '
+        'lblID_guna2
+        '
+        Me.lblID_guna2.BackColor = System.Drawing.Color.Transparent
+        Me.lblID_guna2.ForeColor = System.Drawing.Color.White
+        Me.lblID_guna2.Location = New System.Drawing.Point(94, 17)
+        Me.lblID_guna2.Name = "lblID_guna2"
+        Me.lblID_guna2.Size = New System.Drawing.Size(28, 25)
+        Me.lblID_guna2.TabIndex = 0
+        Me.lblID_guna2.Text = "ID:"
+        '
+        'lblNombre_guna2
+        '
+        Me.lblNombre_guna2.BackColor = System.Drawing.Color.Transparent
+        Me.lblNombre_guna2.ForeColor = System.Drawing.Color.White
+        Me.lblNombre_guna2.Location = New System.Drawing.Point(12, 92)
+        Me.lblNombre_guna2.Name = "lblNombre_guna2"
+        Me.lblNombre_guna2.Size = New System.Drawing.Size(110, 25)
+        Me.lblNombre_guna2.TabIndex = 1
+        Me.lblNombre_guna2.Text = "Nombre(s):"
+        '
+        'lblApellidos_guna2
+        '
+        Me.lblApellidos_guna2.BackColor = System.Drawing.Color.Transparent
+        Me.lblApellidos_guna2.ForeColor = System.Drawing.Color.White
+        Me.lblApellidos_guna2.Location = New System.Drawing.Point(10, 166)
+        Me.lblApellidos_guna2.Name = "lblApellidos_guna2"
+        Me.lblApellidos_guna2.Size = New System.Drawing.Size(112, 25)
+        Me.lblApellidos_guna2.TabIndex = 2
+        Me.lblApellidos_guna2.Text = "Apellido(s):"
+        '
+        'Guna2TextBox1
+        '
+        Me.Guna2TextBox1.Cursor = System.Windows.Forms.Cursors.IBeam
+        Me.Guna2TextBox1.DefaultText = ""
+        Me.Guna2TextBox1.DisabledState.BorderColor = System.Drawing.Color.FromArgb(CType(CType(208, Byte), Integer), CType(CType(208, Byte), Integer), CType(CType(208, Byte), Integer))
+        Me.Guna2TextBox1.DisabledState.FillColor = System.Drawing.Color.FromArgb(CType(CType(226, Byte), Integer), CType(CType(226, Byte), Integer), CType(CType(226, Byte), Integer))
+        Me.Guna2TextBox1.DisabledState.ForeColor = System.Drawing.Color.FromArgb(CType(CType(138, Byte), Integer), CType(CType(138, Byte), Integer), CType(CType(138, Byte), Integer))
+        Me.Guna2TextBox1.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(CType(CType(138, Byte), Integer), CType(CType(138, Byte), Integer), CType(CType(138, Byte), Integer))
+        Me.Guna2TextBox1.FocusedState.BorderColor = System.Drawing.Color.FromArgb(CType(CType(94, Byte), Integer), CType(CType(148, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.Guna2TextBox1.Font = New System.Drawing.Font("Segoe UI", 9.0!)
+        Me.Guna2TextBox1.HoverState.BorderColor = System.Drawing.Color.FromArgb(CType(CType(94, Byte), Integer), CType(CType(148, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.Guna2TextBox1.Location = New System.Drawing.Point(143, 17)
+        Me.Guna2TextBox1.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Guna2TextBox1.Name = "Guna2TextBox1"
+        Me.Guna2TextBox1.PasswordChar = Global.Microsoft.VisualBasic.ChrW(0)
+        Me.Guna2TextBox1.PlaceholderText = ""
+        Me.Guna2TextBox1.SelectedText = ""
+        Me.Guna2TextBox1.Size = New System.Drawing.Size(150, 50)
+        Me.Guna2TextBox1.TabIndex = 3
+        '
+        'Guna2TextBox2
+        '
+        Me.Guna2TextBox2.Cursor = System.Windows.Forms.Cursors.IBeam
+        Me.Guna2TextBox2.DefaultText = ""
+        Me.Guna2TextBox2.DisabledState.BorderColor = System.Drawing.Color.FromArgb(CType(CType(208, Byte), Integer), CType(CType(208, Byte), Integer), CType(CType(208, Byte), Integer))
+        Me.Guna2TextBox2.DisabledState.FillColor = System.Drawing.Color.FromArgb(CType(CType(226, Byte), Integer), CType(CType(226, Byte), Integer), CType(CType(226, Byte), Integer))
+        Me.Guna2TextBox2.DisabledState.ForeColor = System.Drawing.Color.FromArgb(CType(CType(138, Byte), Integer), CType(CType(138, Byte), Integer), CType(CType(138, Byte), Integer))
+        Me.Guna2TextBox2.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(CType(CType(138, Byte), Integer), CType(CType(138, Byte), Integer), CType(CType(138, Byte), Integer))
+        Me.Guna2TextBox2.FocusedState.BorderColor = System.Drawing.Color.FromArgb(CType(CType(94, Byte), Integer), CType(CType(148, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.Guna2TextBox2.Font = New System.Drawing.Font("Segoe UI", 9.0!)
+        Me.Guna2TextBox2.HoverState.BorderColor = System.Drawing.Color.FromArgb(CType(CType(94, Byte), Integer), CType(CType(148, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.Guna2TextBox2.Location = New System.Drawing.Point(143, 92)
+        Me.Guna2TextBox2.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Guna2TextBox2.Name = "Guna2TextBox2"
+        Me.Guna2TextBox2.PasswordChar = Global.Microsoft.VisualBasic.ChrW(0)
+        Me.Guna2TextBox2.PlaceholderText = ""
+        Me.Guna2TextBox2.SelectedText = ""
+        Me.Guna2TextBox2.Size = New System.Drawing.Size(200, 50)
+        Me.Guna2TextBox2.TabIndex = 4
+        '
+        'Guna2TextBox3
+        '
+        Me.Guna2TextBox3.Cursor = System.Windows.Forms.Cursors.IBeam
+        Me.Guna2TextBox3.DefaultText = ""
+        Me.Guna2TextBox3.DisabledState.BorderColor = System.Drawing.Color.FromArgb(CType(CType(208, Byte), Integer), CType(CType(208, Byte), Integer), CType(CType(208, Byte), Integer))
+        Me.Guna2TextBox3.DisabledState.FillColor = System.Drawing.Color.FromArgb(CType(CType(226, Byte), Integer), CType(CType(226, Byte), Integer), CType(CType(226, Byte), Integer))
+        Me.Guna2TextBox3.DisabledState.ForeColor = System.Drawing.Color.FromArgb(CType(CType(138, Byte), Integer), CType(CType(138, Byte), Integer), CType(CType(138, Byte), Integer))
+        Me.Guna2TextBox3.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(CType(CType(138, Byte), Integer), CType(CType(138, Byte), Integer), CType(CType(138, Byte), Integer))
+        Me.Guna2TextBox3.FocusedState.BorderColor = System.Drawing.Color.FromArgb(CType(CType(94, Byte), Integer), CType(CType(148, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.Guna2TextBox3.Font = New System.Drawing.Font("Segoe UI", 9.0!)
+        Me.Guna2TextBox3.HoverState.BorderColor = System.Drawing.Color.FromArgb(CType(CType(94, Byte), Integer), CType(CType(148, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.Guna2TextBox3.Location = New System.Drawing.Point(143, 166)
+        Me.Guna2TextBox3.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Guna2TextBox3.Name = "Guna2TextBox3"
+        Me.Guna2TextBox3.PasswordChar = Global.Microsoft.VisualBasic.ChrW(0)
+        Me.Guna2TextBox3.PlaceholderText = ""
+        Me.Guna2TextBox3.SelectedText = ""
+        Me.Guna2TextBox3.Size = New System.Drawing.Size(200, 50)
+        Me.Guna2TextBox3.TabIndex = 5
+        '
+        'Guna2TextBox4
+        '
+        Me.Guna2TextBox4.Cursor = System.Windows.Forms.Cursors.IBeam
+        Me.Guna2TextBox4.DefaultText = ""
+        Me.Guna2TextBox4.DisabledState.BorderColor = System.Drawing.Color.FromArgb(CType(CType(208, Byte), Integer), CType(CType(208, Byte), Integer), CType(CType(208, Byte), Integer))
+        Me.Guna2TextBox4.DisabledState.FillColor = System.Drawing.Color.FromArgb(CType(CType(226, Byte), Integer), CType(CType(226, Byte), Integer), CType(CType(226, Byte), Integer))
+        Me.Guna2TextBox4.DisabledState.ForeColor = System.Drawing.Color.FromArgb(CType(CType(138, Byte), Integer), CType(CType(138, Byte), Integer), CType(CType(138, Byte), Integer))
+        Me.Guna2TextBox4.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(CType(CType(138, Byte), Integer), CType(CType(138, Byte), Integer), CType(CType(138, Byte), Integer))
+        Me.Guna2TextBox4.FocusedState.BorderColor = System.Drawing.Color.FromArgb(CType(CType(94, Byte), Integer), CType(CType(148, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.Guna2TextBox4.Font = New System.Drawing.Font("Segoe UI", 9.0!)
+        Me.Guna2TextBox4.HoverState.BorderColor = System.Drawing.Color.FromArgb(CType(CType(94, Byte), Integer), CType(CType(148, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.Guna2TextBox4.Location = New System.Drawing.Point(143, 246)
+        Me.Guna2TextBox4.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Guna2TextBox4.Name = "Guna2TextBox4"
+        Me.Guna2TextBox4.PasswordChar = Global.Microsoft.VisualBasic.ChrW(0)
+        Me.Guna2TextBox4.PlaceholderText = ""
+        Me.Guna2TextBox4.SelectedText = ""
+        Me.Guna2TextBox4.Size = New System.Drawing.Size(200, 50)
+        Me.Guna2TextBox4.TabIndex = 6
+        '
+        'lblDireccion_guna2
+        '
+        Me.lblDireccion_guna2.BackColor = System.Drawing.Color.Transparent
+        Me.lblDireccion_guna2.ForeColor = System.Drawing.Color.White
+        Me.lblDireccion_guna2.Location = New System.Drawing.Point(23, 246)
+        Me.lblDireccion_guna2.Name = "lblDireccion_guna2"
+        Me.lblDireccion_guna2.Size = New System.Drawing.Size(99, 25)
+        Me.lblDireccion_guna2.TabIndex = 7
+        Me.lblDireccion_guna2.Text = "Dirección:"
+        '
+        'Guna2PictureBox1
+        '
+        Me.Guna2PictureBox1.ImageRotate = 0!
+        Me.Guna2PictureBox1.Location = New System.Drawing.Point(371, 96)
+        Me.Guna2PictureBox1.Name = "Guna2PictureBox1"
+        Me.Guna2PictureBox1.Size = New System.Drawing.Size(300, 200)
+        Me.Guna2PictureBox1.TabIndex = 8
+        Me.Guna2PictureBox1.TabStop = False
+        '
+        'pboxReferencia_guna2
+        '
+        Me.pboxReferencia_guna2.BackColor = System.Drawing.Color.Transparent
+        Me.pboxReferencia_guna2.ForeColor = System.Drawing.Color.White
+        Me.pboxReferencia_guna2.Location = New System.Drawing.Point(371, 65)
+        Me.pboxReferencia_guna2.Name = "pboxReferencia_guna2"
+        Me.pboxReferencia_guna2.Size = New System.Drawing.Size(113, 25)
+        Me.pboxReferencia_guna2.TabIndex = 9
+        Me.pboxReferencia_guna2.Text = "Referencia:"
+        '
+        'btngradNuevoReg_guna2
+        '
+        Me.btngradNuevoReg_guna2.DisabledState.BorderColor = System.Drawing.Color.DarkGray
+        Me.btngradNuevoReg_guna2.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray
+        Me.btngradNuevoReg_guna2.DisabledState.FillColor = System.Drawing.Color.FromArgb(CType(CType(169, Byte), Integer), CType(CType(169, Byte), Integer), CType(CType(169, Byte), Integer))
+        Me.btngradNuevoReg_guna2.DisabledState.FillColor2 = System.Drawing.Color.FromArgb(CType(CType(169, Byte), Integer), CType(CType(169, Byte), Integer), CType(CType(169, Byte), Integer))
+        Me.btngradNuevoReg_guna2.DisabledState.ForeColor = System.Drawing.Color.FromArgb(CType(CType(141, Byte), Integer), CType(CType(141, Byte), Integer), CType(CType(141, Byte), Integer))
+        Me.btngradNuevoReg_guna2.Font = New System.Drawing.Font("Century Gothic", 12.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btngradNuevoReg_guna2.ForeColor = System.Drawing.Color.White
+        Me.btngradNuevoReg_guna2.Location = New System.Drawing.Point(17, 364)
+        Me.btngradNuevoReg_guna2.Name = "btngradNuevoReg_guna2"
+        Me.btngradNuevoReg_guna2.Size = New System.Drawing.Size(180, 45)
+        Me.btngradNuevoReg_guna2.TabIndex = 10
+        Me.btngradNuevoReg_guna2.Text = "Nuevo"
+        '
+        'Guna2Button1
+        '
+        Me.Guna2Button1.DisabledState.BorderColor = System.Drawing.Color.DarkGray
+        Me.Guna2Button1.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray
+        Me.Guna2Button1.DisabledState.FillColor = System.Drawing.Color.FromArgb(CType(CType(169, Byte), Integer), CType(CType(169, Byte), Integer), CType(CType(169, Byte), Integer))
+        Me.Guna2Button1.DisabledState.ForeColor = System.Drawing.Color.FromArgb(CType(CType(141, Byte), Integer), CType(CType(141, Byte), Integer), CType(CType(141, Byte), Integer))
+        Me.Guna2Button1.Font = New System.Drawing.Font("Century Gothic", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Guna2Button1.ForeColor = System.Drawing.Color.White
+        Me.Guna2Button1.Location = New System.Drawing.Point(263, 364)
+        Me.Guna2Button1.Name = "Guna2Button1"
+        Me.Guna2Button1.Size = New System.Drawing.Size(180, 45)
+        Me.Guna2Button1.TabIndex = 11
+        Me.Guna2Button1.Text = "Guardar"
+        '
+        'btnVerReg_guna2
+        '
+        Me.btnVerReg_guna2.DisabledState.BorderColor = System.Drawing.Color.DarkGray
+        Me.btnVerReg_guna2.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray
+        Me.btnVerReg_guna2.DisabledState.FillColor = System.Drawing.Color.FromArgb(CType(CType(169, Byte), Integer), CType(CType(169, Byte), Integer), CType(CType(169, Byte), Integer))
+        Me.btnVerReg_guna2.DisabledState.ForeColor = System.Drawing.Color.FromArgb(CType(CType(141, Byte), Integer), CType(CType(141, Byte), Integer), CType(CType(141, Byte), Integer))
+        Me.btnVerReg_guna2.Font = New System.Drawing.Font("Century Gothic", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnVerReg_guna2.ForeColor = System.Drawing.Color.White
+        Me.btnVerReg_guna2.Location = New System.Drawing.Point(491, 364)
+        Me.btnVerReg_guna2.Name = "btnVerReg_guna2"
+        Me.btnVerReg_guna2.Size = New System.Drawing.Size(180, 45)
+        Me.btnVerReg_guna2.TabIndex = 12
+        Me.btnVerReg_guna2.Text = "Ver registros"
         '
         'frm_Empleado
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(12.0!, 23.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(66, Byte), Integer), CType(CType(66, Byte), Integer), CType(CType(66, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(682, 453)
+        Me.Controls.Add(Me.btnVerReg_guna2)
+        Me.Controls.Add(Me.Guna2Button1)
+        Me.Controls.Add(Me.btngradNuevoReg_guna2)
+        Me.Controls.Add(Me.pboxReferencia_guna2)
+        Me.Controls.Add(Me.Guna2PictureBox1)
+        Me.Controls.Add(Me.lblDireccion_guna2)
+        Me.Controls.Add(Me.Guna2TextBox4)
+        Me.Controls.Add(Me.Guna2TextBox3)
+        Me.Controls.Add(Me.Guna2TextBox2)
+        Me.Controls.Add(Me.Guna2TextBox1)
+        Me.Controls.Add(Me.lblApellidos_guna2)
+        Me.Controls.Add(Me.lblNombre_guna2)
+        Me.Controls.Add(Me.lblID_guna2)
+        Me.Font = New System.Drawing.Font("Century Gothic", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ForeColor = System.Drawing.SystemColors.ControlLight
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.Name = "frm_Empleado"
-        Me.Text = "Form1"
+        Me.Text = "Sistema de Registros VB.NET v1.0.0.0"
+        CType(Me.Guna2PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
+
+    Friend WithEvents lblID_guna2 As Guna.UI2.WinForms.Guna2HtmlLabel
+    Friend WithEvents lblNombre_guna2 As Guna.UI2.WinForms.Guna2HtmlLabel
+    Friend WithEvents lblApellidos_guna2 As Guna.UI2.WinForms.Guna2HtmlLabel
+    Friend WithEvents Guna2TextBox1 As Guna.UI2.WinForms.Guna2TextBox
+    Friend WithEvents Guna2TextBox2 As Guna.UI2.WinForms.Guna2TextBox
+    Friend WithEvents Guna2TextBox3 As Guna.UI2.WinForms.Guna2TextBox
+    Friend WithEvents Guna2TextBox4 As Guna.UI2.WinForms.Guna2TextBox
+    Friend WithEvents lblDireccion_guna2 As Guna.UI2.WinForms.Guna2HtmlLabel
+    Friend WithEvents Guna2PictureBox1 As Guna.UI2.WinForms.Guna2PictureBox
+    Friend WithEvents pboxReferencia_guna2 As Guna.UI2.WinForms.Guna2HtmlLabel
+    Friend WithEvents btngradNuevoReg_guna2 As Guna.UI2.WinForms.Guna2GradientButton
+    Friend WithEvents Guna2Button1 As Guna.UI2.WinForms.Guna2Button
+    Friend WithEvents btnVerReg_guna2 As Guna.UI2.WinForms.Guna2Button
 End Class
--- crud_vb/capaPresentacion/frm_Empleado.vb
+++ crud_vb/capaPresentacion/frm_Empleado.vb
@@ -1,3 +1,5 @@
 ﻿Public Class frm_Empleado
+    Private Sub txtID_guna2_TextChanged(sender As Object, e As EventArgs)
 
+    End Sub
 End Class
